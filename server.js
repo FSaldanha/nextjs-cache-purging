28bf01c75d341b17c93f696bb5e019294a0637fe
Felipe Saldanha <fgsaldanha@gmail.com>
2021-02-20 16:52:27 -0300

    Minor changes

--- server.js
+++ server.js
@@ -1,39 +1,39 @@
-const { createServer } = require('http')
-const next = require('next')
-const { promises } = require('fs')
+const { createServer } = require('http');
+const next = require('next');
+const { promises } = require('fs');
 
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 const purgeData = async (pathname) => {
-  const fullPathname = `.next/server/pages${pathname}`
-  const fullPathHTML = `${fullPathname}.html`
-  const fullPathJSON = `${fullPathname}.json`
+  const fullPathname = `.next/server/pages${pathname}`;
+  const fullPathHTML = `${fullPathname}.html`;
+  const fullPathJSON = `${fullPathname}.json`;
   try {
-    await promises.unlink(fullPathHTML)
-    await promises.unlink(fullPathJSON)
-    const cachedData = await app.incrementalCache.get(pathname)
-    const staleTime = new Date().getTime() - 1000
+    await promises.unlink(fullPathHTML);
+    await promises.unlink(fullPathJSON);
+    const cachedData = await app.incrementalCache.get(pathname);
+    const staleTime = new Date().getTime() - 1000;
     app.incrementalCache.set(pathname, {
       ...cachedData,
       revalidateAfter: staleTime
-    }, 1)
-    console.log(`Cache of ${fullPathname} was successfully purged`)
+    }, 1);
+    console.log(`Cache of ${fullPathname} was successfully purged`);
   } catch (err) {
-    console.error(`Could not purge cache of ${fullPathname} - ${err}`)
+    console.error(`Could not purge cache of ${fullPathname} - ${err}`);
   }
 }
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const url = new URL(req.url, "http://localhost:3000/")
+    const url = new URL(req.url, "http://localhost:3000/");
     if (url.searchParams.get('purge') == '1') {
       purgeData(url.pathname);
     }
-    handle(req, res)
+    handle(req, res);
   }).listen(3000, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:3000/`)
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:3000/`);
   })
 })
